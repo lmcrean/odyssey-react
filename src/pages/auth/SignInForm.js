270e67e77d577a0e629f26caf702a7cbf88c308b
Laurie Crean <lmcrean@gmail.com>
2024-08-24 15:47:22 +0000

    Implement new fonts (Core Sans C & Serifa) and fix font file paths
    - Added @font-face declarations for Core Sans C and Serifa fonts.
    - Updated variables.css to reference new fonts as primary and secondary fonts.
    - Corrected font file paths to use relative paths within the src/fonts directory for proper resolution during build.
    - Ensured successful build after applying changes.

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -51,7 +51,7 @@ function SignInForm() {
 
   return (
     <Row className={styles.Row}>
-      <Col className="my-auto p-0 p-md-2" md={6}>
+      <Col className="my-auto p-0 p-md-2 m-auto" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>Sign in</h1>
           <Form onSubmit={handleSubmit}>
@@ -107,15 +107,6 @@ function SignInForm() {
             Don't have an account? <span>Sign up now!</span>
           </Link>
         </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
-        />
       </Col>
     </Row>
   );
